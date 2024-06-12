4783394740f54cc8254baf813d749556a1f211c9
Lucas Ribeiro <patruop@gmail.com>
2024-06-12 15:43:31 -0300

    add diagnostics to sidebar

--- config/utils/sidebar.nix
+++ config/utils/sidebar.nix
@@ -21,7 +21,7 @@
         initial_width = 32,
         hide_statusline = false,
         update_interval = 1000,
-        sections = { "files", "git", "containers" },
+        sections = { "files", "diagnostics", "git", "containers" },
         section_separator = {"", "-----", ""},
         section_title_separator = {""},
         containers = {
